9e5d2d0b8b06c3d8a6eb940ca67b01b41ea1bb61
Justin <justinmespel@gmail.com>
2021-06-26 14:31:37 +1200

    App Navigation Routing
    - Basic components and app routing using the navigation bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
 import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
@@ -8,7 +12,16 @@ const routes: Routes = [
   },
   {
     path: 'home', component: HomeComponent
-  }
+  },
+  {
+    path: 'gallery', component: GalleryComponent
+  },
+  {
+    path: 'about', component: AboutComponent
+  },
+  {
+    path: 'contact', component: ContactComponent
+  },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,18 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavBarComponent
+    NavBarComponent,
+    GalleryComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
