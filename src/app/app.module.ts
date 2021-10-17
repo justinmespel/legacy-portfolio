c26a1b0429b780649c6d9b10b62337dbcbe46ba8
Justin Van Der Mespel <justinmespel@gmail.com>
2021-10-17 17:38:33 +1300

    Update Development gallery for public facing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, Title, Meta } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 /* Animations */
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -11,7 +12,6 @@ import player from 'lottie-web';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { GalleryComponent } from './components/gallery/gallery.component';
 import { AboutComponent } from './components/about/about.component';
 import { ContactComponent } from './components/contact/contact.component';
--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
-import { WorkImage } from 'src/app/models/work-image.model';
-import { WorkLink } from 'src/app/models/work-link.model';
-import { WorkTool } from 'src/app/models/work-tool.model';
+import { ViewEncapsulation } from '@angular/core'
+import { NgbCarousel } from '@ng-bootstrap/ng-bootstrap';
+
 import { Work } from 'src/app/models/work.model';
+
 import { AppService } from 'src/app/services/app.service';
 import { MetaService } from 'src/app/services/meta.service';
 
 @Component({
+	encapsulation: ViewEncapsulation.None,
 	selector: 'app-gallery',
 	templateUrl: './gallery.component.html',
 	styleUrls: ['./gallery.component.scss']
