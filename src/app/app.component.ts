e0c5e2ab5b791aa112bc10f7c53fd139458523db
Justin Van Der Mespel <justinmespel@gmail.com>
2021-11-07 21:03:39 +1300

    Mobile Navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,12 @@ import { Component } from '@angular/core';
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'portfolio';
+export class AppComponent  {
+
+	public showMobileMenu: boolean = false;
+
+	public toggleMobileMenu(): void {
+		this.showMobileMenu = !this.showMobileMenu;
+	}
+	
 }
