e5b159323fbe04dbdb39c8622b9a7b7f15a63b61
Justin Van Der Mespel <justinmespel@gmail.com>
2021-11-14 13:51:04 +1300

    Update Code Data

--- src/assets/data/code.data.ts
+++ src/assets/data/code.data.ts
@@ -5,7 +5,7 @@ import { WorkTool } from '../../app/models';
 export const CodeData: Work[] =  [
 	new Work({
 		title: 'Hell Ordering',
-		description: 'Bringing a fresh design to life! It was a blast working on Hell Pizza’s new ordering website. This platform is used by Hell customers all over New Zealand.\n\nWe aimed to create a new, modern web application for their most customer facing website. This involved refreshing their existing web features and implementing exciting new ones. We went for a OOP approach using Angular 10 as this fit their preferred structure.',
+		description: 'Bringing a fresh design to life! It was a blast working on Hell Pizza’s new ordering website. \n\nWe created a brand new ordering application for their most customer facing website. This involved refreshing their existing web features and implementing exciting new ones.',
 		tools: [
 			new WorkTool({ 
 				name: 'Angular 10' 
@@ -46,7 +46,7 @@ export const CodeData: Work[] =  [
 	}),
 	new Work({
 		title: 'Driver App',
-		description: 'Lead developer of Hell’s in-house driver performance Android app - Styx Drive. This was developed using the Nativescript and Angular 8 framework as per the project\'s specifications. \n\n The app communicates with their API to complete various internal tasks. It features QR authentication and implements a custom Java background service for location data.',
+		description: 'Developed Hell’s in-house driver performance Android app - Styx Drive. This was developed using the Nativescript and Angular 8 framework. \n\n The app communicates with the Hell API to complete various internal tasks. It features QR authentication and implements a custom Java background service.',
 		tools: [
 			new WorkTool({ 
 				name: 'Nativescript' 
@@ -75,7 +75,7 @@ export const CodeData: Work[] =  [
 		description: 'A web application utilising the clients existing intranet, built on AngularJS. \n\n A mobile first approach was taken to capture the majority of the target audience. Mixing beautiful design with intuitive functionality allowed me to create an easy to use platform for both the end user and content manager.',
 		tools: [
 			new WorkTool({ 
-				name: 'AngularJS' 
+				name: 'Angular JS' 
 			}),
 			new WorkTool({ 
 				name: 'PHP' 
@@ -89,7 +89,7 @@ export const CodeData: Work[] =  [
 	}),
 	new Work({
 		title: 'Uber Intergration',
-		description: 'Managed development and communication between Uber Eats and Hell to create a seamless platform integration that scales for their increasing number of stores.\n\nI used the existing API and backend, I was able to send the order striaght to their kitchens. Logging is setup throughout the order\'s build proccess to help monitor the integration health. ',
+		description: 'Managed full development and communication between Uber Eats and Hell to create a seamless platform integration that scales for their increasing number of stores.\n\nI was able to adapt Hell\'s existing infastructure to send the order striaght to their kitchens. Logging is setup throughout the order\'s build proccess to help monitor the integration health. ',
 		tools: [ 
 			new WorkTool({ 
 				name: 'PHP'
@@ -98,7 +98,7 @@ export const CodeData: Work[] =  [
 				name: 'MySql'
 			}),
 			new WorkTool({ 
-				name: 'Postman' 
+				name: 'Angular JS' 
 			}),
 			new WorkTool({ 
 				name: 'Phinx' 
@@ -112,10 +112,10 @@ export const CodeData: Work[] =  [
 	}),
 	new Work({
 		title: 'Roster',
-		description: 'A in-house roster used by store\'s to create their weekly shifts. The roster ties in other features such as leave requests and upcoming special events to help the user create a balanced work flow for each day.',
+		description: 'A in-house roster used by store\'s to create their weekly workflow. The roster also ties in leave requests and upcoming special events to help the user create a balanced work flow for each day.',
 		tools: [
 			new WorkTool({ 
-				name: 'AngularJS' 
+				name: 'Angular JS' 
 			}),
 			new WorkTool({ 
 				name: 'MySQL' 
